c3ea28007d6c20a279de4740d838567c49292ecd
changruochuan <tengzijian789@Ebs>
2020-05-07 16:00:41 +0800

    redis提交

---
+++ redis-demo/src/main/java/com/crc/redisdemo/pojo/UserVo.java
@@ -0,0 +1,49 @@
+package com.crc.redisdemo.pojo;
+
+/**
+ * @Author CRC
+ * @Date 2020/5/7
+ * @Describe
+ */
+public class UserVo {
+
+    public  static final String Table = "t_user";
+
+    private String name;
+    private String address;
+    private Integer age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+
+    @Override
+    public String toString() {
+        return "UserVo{" +
+                "name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
