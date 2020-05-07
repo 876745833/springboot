d36f27a6d35af340660c3b0b88e18400e072ecc2
changruochuan <tengzijian789@Ebs>
2020-05-07 13:59:07 +0800

    swagger提交

---
+++ swagger-demo/src/main/java/com/crc/swaggerdemo/controller/TestController.java
@@ -0,0 +1,49 @@
+package com.crc.swaggerdemo.controller;
+
+import io.swagger.annotations.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * @Author CRC
+ * @Date 2020/5/7
+ * @Describe
+ */
+@Api(tags = "swagger-api测试")
+@RestController
+public class TestController {
+
+    @GetMapping("swagger")
+    @ApiOperation(value = "测试get", notes = "测试get")
+    public String swgetTest(){
+        return "get success";
+    }
+
+    @PostMapping("swagger")
+    @ApiOperation(value = "测试post", notes = "测试post")
+    public String swpostTest(){
+        return "post success";
+    }
+
+    @DeleteMapping("swagger")
+    @ApiOperation(value = "测试delete", notes = "测试delete")
+    public String swdeleteTest(){
+        return "delete success";
+    }
+
+    @PutMapping("swagger")
+    @ApiOperation(value = "测试put", notes = "测试put")
+    public String swputTest(){
+        return "put success";
+    }
+
+    @ApiOperation(value = "登录" ,  notes="登录 接口")
+    @RequestMapping(path = "/login" , method = {RequestMethod.POST})
+    public String login2(@RequestParam(value = "name" ) @ApiParam(value = "账号",required = true) String name,
+                         @RequestParam(value = "pwd" ) @ApiParam(value = "密码",required = true) String pwd) throws Exception {
+        return "登录账号为 "+name +"<-->"+ pwd;
+    }
+
+
+}
